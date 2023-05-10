ae17368192a4b7b545fe0fcbacb747168c4c1739
AlexKondrukhov <124981539+AlexKondrukhov@users.noreply.github.com>
2023-05-10 16:18:11 +0500

    Update and rename index.html to index.js

---
+++ js/index.js
@@ -0,0 +1,9 @@
+
+let burgerMenu = document.querySelector('.header__burger-btn');
+let burgerList = document.querySelector('.header');
+
+burgerMenu.addEventListener('click', mobileBurger);
+
+function mobileBurger() {
+    burgerList.classList.toggle('open');
+}
